855a377202893ecbe5f9f4fb4080799d8a30a106
Andrew Goldstein <andrewg3311@gmail.com>
2018-10-30 12:56:37 -0500

    Updating to use exact value of pi0

--- code/dsc-normalmeans/analyze_dsc_normal_means.R
+++ code/dsc-normalmeans/analyze_dsc_normal_means.R
@@ -1,8 +1,39 @@
 library(dscrutils)
+library(dplyr)
+library(tidyr)
+library(ggplot2)
 
 dscout = dscquery(dsc.outdir = "normal_means", 
                   targets = c("simulate.true_pi0", "simulate.true_a", "simulate.true_mu", 
                               "eb.prior", "eb.fix_mu", "eb.g_pi0", "eb.g_a", "eb.g_mu", 
                               "eb.loglik", "score.RMSE"))
 
-View(dscout)
+dscout_wide = dscout %>% spread(score, score.RMSE)
+View(dscout_wide)
+
+labels_pi0 = character(length(unique(dscout_wide$simulate.true_pi0)))
+for (i in 1:length(labels_pi0)) {
+  labels_pi0[i] = paste("pi0 = ", unique(dscout_wide$simulate.true_pi0)[i], sep = "")
+  names(labels_pi0)[i] = unique(dscout_wide$simulate.true_pi0)[i]
+}
+labels_a = character(length(unique(dscout_wide$simulate.true_a)))
+for (i in 1:length(labels_a)) {
+  labels_a[i] = paste("a = ", unique(dscout_wide$simulate.true_a)[i], sep = "")
+  names(labels_a)[i] = unique(dscout_wide$simulate.true_a)[i]
+}
+labels_fix_mu = character(length(unique(dscout_wide$eb.fix_mu)))
+for (i in 1:length(labels_fix_mu)) {
+  labels_fix_mu[i] = paste("fix_mu = ", unique(dscout_wide$eb.fix_mu)[i], sep = "")
+  names(labels_fix_mu)[i] = unique(dscout_wide$eb.fix_mu)[i]
+}
+
+ggplot(dscout_wide, aes(x = factor(simulate.true_a), y = eb.g_a)) +
+  geom_boxplot() + facet_grid(eb.fix_mu ~ simulate.true_pi0, labeller = labeller(simulate.true_pi0 = labels_pi0, eb.fix_mu = labels_fix_mu))
+
+
+ggplot(dscout_wide, aes(x = factor(simulate.true_pi0), y = eb.g_pi0)) +
+  geom_boxplot() + facet_grid(eb.fix_mu ~ simulate.true_a, labeller = labeller(simulate.true_a = labels_a, eb.fix_mu = labels_fix_mu))
+
+
+ggplot(filter(dscout_wide, eb.fix_mu == 1), aes(x = factor(simulate.true_mu), y = eb.g_mu)) +
+  geom_boxplot() + facet_grid(simulate.true_pi0 ~ simulate.true_a, labeller = labeller(simulate.true_a = labels_a, simulate.true_a = labels_pi0))
--- code/dsc-normalmeans/datamaker.R
+++ code/dsc-normalmeans/datamaker.R
@@ -1,7 +1,7 @@
 rnormmix_datamaker = function(args) {
   set.seed(args$seed)
-  non_null = rbinom(args$n, 1, (1 - args$pi0))
-  true_pi0 = 1 - mean(non_null)
+  non_null = (1:args$n) > args$n * args$pi0 # just set first n*pi0 to be null
+  true_pi0 = 1 - mean(non_null) # should be equal to args$pi0
   theta = non_null * rnorm(args$n, 0, 1/sqrt(args$a)) + args$mu
   meta = list(theta = theta, true_pi0 = true_pi0, true_a = args$a, true_mu = args$mu)
   
